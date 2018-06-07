1ef1b6570c7fd0383c25861e6e329edc34dadbb0
Nikos Paz <nikospaz@hotmail.com>
2018-06-07 17:20:06 +0300

    upload partB

--- partB.py
+++ partB.py
@@ -1,34 +1,56 @@
 import pandas as pd
-import re
+import numpy as np
+from sklearn.cluster import KMeans
 
 # TODO: Parse correctly
 # train = pa.read_table("./Delicious/train-data.dat", delimiter=' ', header=None)
 # test = pa.read_table("./Delicious/test-data.dat", delimiter=' ', header=None)
 
+x_train = pd.read_table("train.dat", encoding='utf_8', header=None)
+x_test = pd.read_table("test.dat", encoding='utf_8', header=None)
+
+x_train = x_train.replace(np.nan, 0)
+x_test = x_test.replace(np.nan, 0)
+
 df = pd.read_table('./Delicious/train-label.dat', delimiter=' ', header=None)
+df2 = pd.read_table('./Delicious/test-label.dat', delimiter=' ', header=None)
 df1 = df.sum(axis=0)
 maxClass = df1.argmax()
-# print(df.iloc[:, maxClass])
-df2 = df.iloc[:, maxClass]
-print(df2)
-for x in range(0, len(df2.index)):
-    print(df2.iloc[x])
-# train = pd.read_table('./Delicious/train.dat', delimiter=' ', header=None)
-# train.info()
 
-# train = pd.concat([df2, train], axis=1)
-# train.info()
+y_train = df.iloc[:, maxClass]
+y_test = df2.iloc[:, maxClass]
 
+train_bags = x_train[:-1]
+train_labels = y_train
+test_bags = x_test[:-1]
+test_labels = y_test
 
-filepath = './Delicious/train-data.dat'
-with open(filepath) as fp:
-    line = fp.readline()
-    line = re.sub('<.*?>', '', line)
-    f = open('./Delicious/train.dat', 'w+')
-    while line:
-        for x in range(0, len(df2.index)):
-            print(str(df2.iloc[x]) + " " + line.strip())
-            f.write(str(df2.iloc[x]) + " " + line.strip() + "\n")
-            line = fp.readline()
-            line = re.sub('<.*?>', '', line)
-f.close()
+# from sklearn.ensemble import RandomForestRegressor
+#
+# rf = RandomForestRegressor(n_estimators=10, random_state=42)
+# rf.fit(train_bags, train_labels)
+# predictions = rf.predict(test_bags)
+# errors = abs(predictions - test_labels)
+# print('Mean Absolute Error:', round(np.mean(errors), 2), 'degrees.')
+# mape = 100 * (errors / test_labels)
+# accuracy = 100 - np.mean(mape)
+# print('Accuracy:', round(accuracy, 2), '%.')
+
+# from sklearn.ensemble import AdaBoostClassifier
+# from sklearn.tree import DecisionTreeClassifier
+#
+# dt = DecisionTreeClassifier()
+# clf = AdaBoostClassifier(n_estimators=100, base_estimator=dt,learning_rate=1)
+# clf.fit(x_train[:-1],y_train)
+
+# from sklearn.naive_bayes import MultinomialNB
+# clf = MultinomialNB().fit(X_train_tfidf, twenty_train.target)
+# text_clf = text_clf.fit(twenty_train.data, twenty_train.target)
+# twenty_test = fetch_20newsgroups(subset='test', shuffle=True)
+# predicted = text_clf.predict(twenty_test.data)
+# np.mean(predicted == twenty_test.target)
+
+# from sklearn.linear_model import SGDClassifier
+# text_clf_svm = text_clf_svm.fit(twenty_train.data, twenty_train.target)
+# predicted_svm = text_clf_svm.predict(twenty_test.data)
+# np.mean(predicted_svm == twenty_test.target)
--- preprocess.py
+++ preprocess.py
@@ -1,21 +1,23 @@
 import pandas as pd
 import re
+import numpy as np
 
 df = pd.read_table('./Delicious/train-label.dat', delimiter=' ', header=None)
 df1 = df.sum(axis=0)
 maxClass = df1.argmax()
 # print(df.iloc[:, maxClass])
 df2 = df.iloc[:, maxClass]
+print(df2.dtypes)
 
 filepath = './Delicious/train-data.dat'
 with open(filepath) as fp:
     line = fp.readline()
     line = re.sub('<.*?>', '', line)
-    f = open('./Delicious/train.dat', 'w+')
+    f = open('./Delicious/trainSet.dat', 'w+')
     while line:
         for x in range(0, len(df2.index)):
-            print(str(df2.iloc[x]) + " " + line.strip())
-            f.write(str(df2.iloc[x]) + " " + line.strip() + "\n")
+            print(str(df2.iloc[x]) + "," + line.strip())
+            f.write(line.strip() + "\n")
             line = fp.readline()
             line = re.sub('<.*?>', '', line)
 f.close()
@@ -31,11 +33,79 @@ filepath = './Delicious/test-data.dat'
 with open(filepath) as fp:
     line = fp.readline()
     line = re.sub('<.*?>', '', line)
-    f = open('./Delicious/test.dat', 'w+')
+    f = open('./Delicious/testSet.dat', 'w+')
     while line:
         for x in range(0, len(df2.index)):
             print(str(df2.iloc[x]) + " " + line.strip())
-            f.write(str(df2.iloc[x]) + " " + line.strip() + "\n")
+            f.write(line.strip() + "\n")
             line = fp.readline()
             line = re.sub('<.*?>', '', line)
 f.close()
+
+# infile = filepath
+# outfile = "cleaned_file.csv"
+#
+# fin = open(infile)
+# fout = open(outfile, "w+")
+# for line in fin:
+#     line = re.sub('<.*?>', '', line)
+#     fout.write(line)
+# fin.close()
+# fout.close()
+train = pd.read_table("./Delicious/train.dat", encoding='utf_8', header=None)
+# data = pd.DataFrame
+# for x in range(0, 10):
+#     print(train.iloc[x])
+# print(train)
+# for x in range (8252):
+#     for y in range(10):
+#         print(x,y)
+# vocabs 8520
+# docs 8252
+matrix = np.zeros((8252, 8520), dtype=int)
+matrix1 = np.zeros((3984, 8520), dtype=int)
+# for i in range(20):
+#     for j in range(8520):
+#         if matrix[i][j] > 2:
+#             matrix[i][j] = 0
+#         print(matrix[i][j], end=' ')
+#         c += 1
+#     print()
+
+with open('./Delicious/trainSet.dat', 'r') as fp:
+    line = fp.readline()
+    cnt = 0
+    while line:
+        for word in line.split():
+            a = int(word)
+            matrix[cnt][a] = 1
+        line = fp.readline()
+        cnt += 1
+
+f = open('train.dat', 'w+')
+for i in range(8252):
+    for j in range(8520):
+        if matrix[i][j] == 1:
+            f.write(str(1) + "\t")
+        else:
+            f.write(str(0) + "\t")
+    f.write("\n")
+
+with open('./Delicious/testSet.dat', 'r') as fp:
+    line = fp.readline()
+    cnt = 0
+    while line:
+        for word in line.split():
+            a = int(word)
+            matrix1[cnt][a] = 1
+        line = fp.readline()
+        cnt += 1
+
+f = open('test.dat', 'w+')
+for i in range(3984):
+    for j in range(8520):
+        if matrix1[i][j] == 1:
+            f.write(str(1) + "\t")
+        else:
+            f.write(str(0) + "\t")
+    f.write("\n")
